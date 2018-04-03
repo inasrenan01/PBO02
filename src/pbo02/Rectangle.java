626795ccad923f5db813ee6f3f627afb1de61f03
inasrenan01 <xidorenan@gmail.com>
2018-04-03 22:34:22 +0800

    membuat atribut dan method

--- src/pbo02/Rectangle.java
+++ src/pbo02/Rectangle.java
@@ -1,5 +1,13 @@
 package pbo02;
 
 public class Rectangle {
+    double panjang;
+    double lebar;
     
+    void cetakInfo(){
+        System.out.println("==================");
+        System.out.println("Panjang :"+panjang);
+        System.out.println("Lebar   :"+lebar);
+        System.out.println("==================");
+    }
 }
