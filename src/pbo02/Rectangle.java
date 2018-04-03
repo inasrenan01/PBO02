6228c5659c0cfe065f32d4f91de59ea952002f03
inasrenan01 <xidorenan@gmail.com>
2018-04-03 22:38:10 +0800

    mengisi nilai atribut

--- src/pbo02/Rectangle.java
+++ src/pbo02/Rectangle.java
@@ -10,4 +10,25 @@ public class Rectangle {
         System.out.println("Lebar   :"+lebar);
         System.out.println("==================");
     }
+
+double hitungluas(){
+        double luas;
+        luas=panjang*lebar;
+        return luas;
+    }
+    
+    void cetakLuas(){
+        System.out.println("Luasnya adalah :"+hitungluas());
+    }
+
+    public Rectangle() {
+        panjang=5;
+        lebar=2;
+    }
+
+    public Rectangle(double panjang, double lebar) {
+        this.panjang = panjang;
+        this.lebar = lebar;
+    }
+    
 }
